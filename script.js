c9c311274de0d57fa66fae45293a260f6296aa70
Dragoş <edragos33@gmail.com>
2025-05-19 18:55:37 +0200

    diaporama added

---
+++ script.js
@@ -0,0 +1,19 @@
+const imgContainer = document.querySelectorAll(".img-container");
+const img = document.querySelectorAll(".image")
+const rightBtn = document.querySelector(".right-btn");
+const leftBtn = document.querySelector(".left-btn");
+let currentIndex = 0;
+
+rightBtn.addEventListener("click", function(){
+    img[currentIndex].classList.add("hidden");
+    currentIndex = (currentIndex + 1) % img.length;
+    img[currentIndex].classList.remove("hidden");
+    console.log("click")
+});
+
+leftBtn.addEventListener("click", function(){
+    img[currentIndex].classList.add("hidden");
+    currentIndex = (currentIndex - 1 + img.length) % img.length;
+    img[currentIndex].classList.remove("hidden");
+    console.log("click")
+});
